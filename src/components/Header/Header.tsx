1c1996b4e0024f522642c71fc486173ef0ea7ffe
Vladimir <veb455@mail.ru>
2025-02-25 16:40:08 +0300

    display header aft scroll

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,35 @@
-import React, { FC, JSX } from "react";
+import React, { FC, JSX, useEffect, useState } from "react";
 import style from "./style.module.css";
 import Logo from "../Logo/Logo.tsx";
 // import MenuBtn from "../MenuBtn/MenuBtn.tsx";
 import LinkList from "../LinkList/LinkList.tsx";
 
 const HeaderComponent: FC = (): JSX.Element => {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY > 300){
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false)
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return() => window.removeEventListener("scroll", handleScroll);
+  }, [])
+
   return (
-      <header className={style.Header}>
+    <>
+      <div className={style.invisibleBlock}></div>
+      <header className={isScrolled ? `${style.Header} ${style.Sticky}` : style.Header}>
         <Logo/>
         {/* <MenuBtn></MenuBtn> */}
         <LinkList/>
       </header>
+    </>
   );
 };
 
